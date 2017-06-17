f75f679ec023e8f79c7a930dcbdbf5d09d111531
Vishal Arora <vishal@vishalarora.com>
2017-06-17 02:45:24 -0400

    Permit saving/updating `list_id` for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -53,6 +53,6 @@ class TasksController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def task_params
-      params.require(:task).permit(:description, :due_date, :complete)
+      params.require(:task).permit(:description, :due_date, :complete, :list_id)
     end
 end
