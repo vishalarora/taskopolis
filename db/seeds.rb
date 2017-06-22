2eae79781dc3e4c2078526097ce4992949ad342f
Vishal Arora <vishalarora@users.noreply.github.com>
2017-06-22 12:55:26 -0400

    Add DB migrations and demo data to deployments (#19)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "ffaker"
+
+# Clear the DB
+User.destroy_all
+
+# Users
+User.create(email: "josh@joshstricklin.com",
+            password: "p@ssw0rd",
+            password_confirmation: "p@ssw0rd")
+
+User.create(email: "vishal@vishalarora.com",
+            password: "p@ssw0rd",
+            password_confirmation: "p@ssw0rd")
+
+# Lists & Tasks
+4.times do
+  user = User.order("RANDOM()").first
+  list = List.create(name: FFaker::Skill.specialty, user_id: user.id)
+
+  rand(2..6).times do
+    Task.create(description: FFaker::Company.bs,
+                due_date: FFaker::Time.between(Time.now, Time.now + 1.year),
+                complete: false,
+                list_id: list.id,
+                user_id: user.id)
+  end
+end
