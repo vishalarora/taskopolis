880b179fd6bae78f14a02e2b4951aa0037cf67a0
Vishal Arora <vishal@vishalarora.com>
2017-06-23 12:52:22 -0400

    Convert datetime column to date

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,13 +13,13 @@ User.create(email: "vishal@vishalarora.com",
             password_confirmation: "p@ssw0rd")
 
 # Lists & Tasks
-4.times do
+6.times do
   user = User.order("RANDOM()").first
   list = List.create(name: FFaker::Skill.specialty, user_id: user.id)
 
   rand(2..6).times do
     Task.create(description: FFaker::Company.bs,
-                due_date: FFaker::Time.between(Time.now, Time.now + 1.year),
+                due_date: FFaker::Time.between(Time.now - 2.days, Time.now + 1.year).to_date,
                 complete: false,
                 list_id: list.id,
                 user_id: user.id)
