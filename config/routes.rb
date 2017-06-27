8a8d575c68ad93e38f3eae9b65b3083e21f03422
Josh Stricklin <jostricklin@gmail.com>
2017-06-27 13:52:09 -0500

    create registrations controller that accepts names

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :lists
   resources :tasks
 
-  devise_for :users
+  devise_for :users, :controllers => {registrations: 'registrations'}
 
   root to: "lists#index"
 end
