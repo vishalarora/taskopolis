39fd3e03ce9da1798c215ba7cf7b56240b5c63e3
Vishal Arora <vishal@vishalarora.com>
2017-06-29 14:25:47 -0400

    Add job to process past due tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,19 @@ class Task < ApplicationRecord
 
   validates :description, presence: true
   validates :due_date, presence: true
+
+  after_create :set_last_synced_date
+
+  def self.past_due
+    where("due_date < ?", Date.today).where("last_synced_at < ?", Date.today)
+  end
+
+  def self.completed
+    where(complete: true)
+  end
+
+  def set_last_synced_date
+    self.update!(last_synced_at: DateTime.now)
+  end
+
 end
