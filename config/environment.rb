7f546256db71b37aa6dc624c1d6031f070d2bc64
Josh Stricklin <jostricklin@gmail.com>
2017-06-30 14:33:40 -0500

    use correct variable names

--- config/environment.rb
+++ config/environment.rb
@@ -1,2 +1,12 @@
 require_relative 'application'
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => ENV['DEFAULT_MAILER_DOMAIN'],
+  :address => ENV['DEFAULT_SEND_FROM_ADDRESS'],
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
