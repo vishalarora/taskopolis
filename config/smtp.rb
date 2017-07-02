c7f34e76215c6626977daf8b72e5b00f8440a0a0
Josh Stricklin <jostricklin@gmail.com>
2017-07-02 18:17:03 -0500

    use correct SMTP settings

--- config/smtp.rb
+++ config/smtp.rb
@@ -1,12 +1,12 @@
-# SMTP_SETTINGS = {
-#   # address: ENV.fetch("SMTP_ADDRESS"), # example: "smtp.sendgrid.net"
-#   # authentication: :plain,
-#   # domain: ENV.fetch("SMTP_DOMAIN"), # example: "heroku.com"
-#   # enable_starttls_auto: true,
-#   # password: ENV.fetch("SMTP_PASSWORD"),
-#   # port: "587",
-#   # user_name: ENV.fetch("SMTP_USERNAME")
-# }
+SMTP_SETTINGS = {
+  address: ENV.fetch("DEFAULT_SEND_FROM_ADDRESS"), # example: "smtp.sendgrid.net"
+  authentication: :plain,
+  domain: ENV.fetch("DEFAULT_MAILER_DOMAIN"), # example: "heroku.com"
+  enable_starttls_auto: true,
+  password: ENV.fetch("SENDGRID_PASSWORD"),
+  port: "587",
+  user_name: ENV.fetch("SENDGRID_USERNAME")
+}
 
 if ENV["EMAIL_RECIPIENTS"].present?
   Mail.register_interceptor RecipientInterceptor.new(ENV["EMAIL_RECIPIENTS"])
