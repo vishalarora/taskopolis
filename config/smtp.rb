450f8d1542962d0e7ad0d87226a3f3f2779c250f
Josh Stricklin <jostricklin@gmail.com>
2017-07-03 14:16:14 -0500

    remove redundant smtp settings and call to them

--- config/smtp.rb
+++
@@ -1,9 +0,0 @@
-SMTP_SETTINGS = {
-  address: ENV.fetch("SMTP_ADDRESS"), # example: "smtp.sendgrid.net"
-  authentication: :plain,
-  domain: ENV.fetch("DEFAULT_MAILER_DOMAIN"), # example: "heroku.com"
-  enable_starttls_auto: true,
-  password: ENV.fetch("SENDGRID_PASSWORD"),
-  port: "587",
-  user_name: ENV.fetch("SENDGRID_USERNAME")
-}
