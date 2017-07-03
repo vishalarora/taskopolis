8de184d7ba3da71e7be67e06fda1801fb9e908ea
Josh Stricklin <jostricklin@gmail.com>
2017-07-03 13:55:43 -0500

    user correct vars

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,8 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
-  :domain => ENV['SMTP_ADDRESS'],
-  :address => ENV['DEFAULT_SEND_FROM_ADDRESS'],
+  :domain => ENV['DEFAULT_MAILER_DOMAIN'],
+  :address => ENV['SMTP_ADDRESS'],
   :port => 587,
   :authentication => :plain,
   :enable_starttls_auto => true
