bc838a7a709d7a5ee931f4b20aee1127ff709ce0
Josh Stricklin <jostricklin@gmail.com>
2017-07-15 14:29:37 -0500

    define params

--- app/controllers/task_actions_controller.rb
+++ app/controllers/task_actions_controller.rb
@@ -2,6 +2,7 @@ class TaskActionsController < ApplicationController
 before_action :set_task, only: [:create, :destroy, :toggle_complete]
 
   def create
+    @task_action = TaskAction.new(task_action_params)
   end
 
   def destroy
@@ -9,13 +10,14 @@ before_action :set_task, only: [:create, :destroy, :toggle_complete]
 
   private
 
-  def toggle_complete
-  end
-
   def set_task
     @task = task.find.(params[:id])
   end
 
   def task_action_params
+    params.require(:task_action).permit(:description)
+  end
+
+  def toggle_complete
   end
 end
